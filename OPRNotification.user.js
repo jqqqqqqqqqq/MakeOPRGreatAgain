a2c21618f196feb5cc830cdf071cbb328621eb69
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-10-17 14:51:27 +0800

    Update OPRNotification.user.js

--- OPRNotification.user.js
+++ OPRNotification.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OPR Notification
-// @version      0.2
+// @version      0.3
 // @description  pull latest info every 10 seconds, if recon is available, send a web notification.
 // @updateURL    https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/OPRNotification.user.js
 // @downloadURL  https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/OPRNotification.user.js
@@ -15,14 +15,20 @@
 //////////DO NOT EDIT THIS LINE BELOW!
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
+function rnd(start, end){
+    return Math.floor((Math.random() * (end - start) + start));
+}
 
 
 (function() {
     var injector = angular.injector(['ng']);
     injector.invoke(function($http){
         var first_try = true;
-        var interval=self.setInterval(function(){
+        var flip = 0;
+        var timeout=rnd(20,60)*1000;
+        
+        flip=setTimeout(function timeoutFun(){
+            
             var SUBMISSION_URL = "/api/v1/vault/review";
             var items = {};
             $http.get(SUBMISSION_URL).then(function(response) {
@@ -42,7 +48,36 @@
                     });
                 }
                 first_try = false;
-            });},10000);
-
+            });
+            
+            timeout=rnd(20,60)*1000;
+            flip=setTimeout(timeoutFun,timeout);
+        },timeout);
+/*        
+        var interval=self.setInterval(function(){
+            rd=rnd(20,60);
+            if(rnd==1)
+                return;
+            var SUBMISSION_URL = "/api/v1/vault/review";
+            var items = {};
+            $http.get(SUBMISSION_URL).then(function(response) {
+                items = response.data;
+                console.log(items.code);
+                if(items.code == "OK"){
+                    clearInterval(interval);
+                    if(first_try)
+                        return;
+                    Notification.requestPermission( function(status) {
+                        console.log(status); // 仅当值为 "granted" 时显示通知
+                        var new_portal = new Notification("New portals available！",{// 显示通知
+                            body: 'Come on! Let\'s continue.',
+                            icon:"/img/great.png"
+                        });
+                        location.href="/"; //有po就去主页
+                    });
+                }
+                first_try = false;
+            });},30000);
+*/
     });
 })();
