810a30d8d8232933c35356ea805f8f1ef23293fa
jqqqqqqqqqq <jqqqqqqqqqq@qq.com>
2017-10-20 21:33:03 +0800

    Update OPRNotification.user.js

--- OPRNotification.user.js
+++ OPRNotification.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         OPR Notification
-// @version      0.4
+// @version      0.5
 // @description  pull latest info every 10 seconds, if recon is available, send a web notification.
 // @updateURL    https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/OPRNotification.user.js
 // @downloadURL  https://github.com/jqqqqqqqqqq/MakeOPRGreatAgain/raw/master/OPRNotification.user.js
@@ -21,24 +21,29 @@ function rnd(start, end){
 
 
 (function() {
+    Notification.requestPermission();
+    console.log("OPR Notification running...");
     var injector = angular.injector(['ng']);
     injector.invoke(function($http){
         var first_try = true;
         var flip = 0;
         var timeout=rnd(20,60)*1000;
-        
         flip=setTimeout(function timeoutFun(){
-            
             var SUBMISSION_URL = "/api/v1/vault/review";
             var items = {};
             $http.get(SUBMISSION_URL).then(function(response) {
                 items = response.data;
-                console.log(items.code);
+                if(items.code == "ERROR"){
+                    timeout=rnd(20,60);
+                    console.log("No portal available, retry in " + timeout + "seconds");
+                    flip=setTimeout(timeoutFun,timeout*1000);
+                }
+                
                 if(items.code == "OK"){
                     if(first_try)
                         return;
                     Notification.requestPermission( function(status) {
-                        console.log(status); // 仅当值为 "granted" 时显示通知
+                        console.log("New portals available！"); // 仅当值为 "granted" 时显示通知
                         var new_portal = new Notification("New portals available！",{// 显示通知
                             body: 'Come on! Let\'s continue.',
                             icon:"/img/great.png"
@@ -47,8 +52,6 @@ function rnd(start, end){
                     });
                 }
                 first_try = false;
-                timeout=rnd(20,60)*1000;
-                flip=setTimeout(timeoutFun,timeout);
             });
         },timeout);
     });
